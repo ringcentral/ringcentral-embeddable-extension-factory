f041b42fb7ed90334e643c25decfb34cdf9b1987
ZHAO Xudong <zxdong@gmail.com>
2018-10-08 08:42:55 +0800

    fix #17 fix background.js

--- templates/dist/background.js
+++ templates/dist/background.js
@@ -1,49 +1,36 @@
+function checkTab(tab) {
+  return tab &&
+    tab.url &&
+    tab.url.startsWith('http')
+    // todo add more rules
+}
 
-let newWindow
+async function cb(tabId) {
+  let tab = await new Promise((resolve) => {
+    chrome.tabs.get(tabId, resolve)
+  })
+  if (
+    checkTab(tab)
+  ) {
+    chrome.pageAction.show(tab.id)
+    return
+  }
+}
+
+chrome.tabs.onCreated.addListener(cb)
+chrome.tabs.onUpdated.addListener(cb)
 
 chrome.pageAction.onClicked.addListener(function (tab) {
-  // open float app window when click icon in office page
+  chrome.pageAction.show(tab.id)
   if (
-    tab &&
-    tab.url &&
-    tab.url.startsWith('https://app.hubspot.com') &&
-    !tab.url.startsWith('https://app.hubspot.com/login') &&
-    !tab.url.startsWith('https://app.hubspot.com/myaccounts-beta')
+    checkTab(tab)
   ) {
     // send message to content.js to to open app window.
     chrome.tabs.sendMessage(tab.id, { action: 'openAppWindow' }, function(response) {
       console.log(response)
     })
-    chrome.pageAction.show(tab.id)
     return
   }
 })
 
-// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-//   let {
-//     method,
-//     key,
-//     value
-//   } = request
-//   if (method === 'getLocalStorage') {
-//     sendResponse({
-//       value: localStorage.getItem(key),
-//       key
-//     })
-//   } else if (method === 'setLocalStorage') {
-//     localStorage.setItem(key, value)
-//     sendResponse({})
-//   } else if (method === 'removeLocalStorage') {
-//     localStorage.removeItem(key)
-//     sendResponse({})
-//   } else {
-//     sendResponse({})
-//   }
-// })
-
-chrome.windows.onRemoved.addListener(function (id) {
-  if (newWindow && newWindow.id === id) {
-    newWindow = null
-  }
-})
 
